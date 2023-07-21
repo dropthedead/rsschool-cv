f5e1938458d07fe795fa16550a3babf44992970d
dropthedead <dropdeaddropdead@yandex.ru>
2023-07-21 13:45:49 +0300

    fix: smooth js

--- library/index.js
+++ library/index.js
@@ -1,37 +1,28 @@
-document.querySelector(".input_text._name").addEventListener("keyup", function(){
+document
+  .querySelector(".input_text._name")
+  .addEventListener("keyup", function () {
     this.value = this.value.replace(/[^[a-zA-Z \d -]/g, "");
-});
-document.querySelector(".input_text._id").addEventListener("keyup", function(){
+  });
+document
+  .querySelector(".input_text._id")
+  .addEventListener("keyup", function () {
     this.value = this.value.replace(/[^[a-zA-Z \d -]/g, "");
-});
-const anchors = [].slice.call(document.querySelectorAll('a[href*="#"]')),
-      animationTime = 700,
-      framesCount = 20;
-
-anchors.forEach(function(item) {
- 
-  item.addEventListener('click', function(e) {
-    
+  });
+const scrollToLink = (link) => {
+  const id = link.getAttribute("href").slice(1);
+  const element = document.getElementById(id);
+  if (element) {
+    element.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest",
+    });
+  }
+};
+const links = document.querySelectorAll('a[href*="#"]');
+links.forEach((link) => {
+  link.addEventListener("click", function (e) {
     e.preventDefault();
-    
-   
-    let coordY = document.querySelector(item.getAttribute('href')).getBoundingClientRect().top + window.pageYOffset;
-    
-    
-    let scroller = setInterval(function() {
-      
-      let scrollBy = coordY / framesCount;
-      
-      
-      if(scrollBy > window.pageYOffset - coordY && window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
-       
-        window.scrollBy(0, scrollBy);
-      } else {
-        
-        window.scrollTo(0, coordY);
-        clearInterval(scroller);
-      }
-    
-    }, animationTime / framesCount);
+    scrollToLink(link);
   });
 });
